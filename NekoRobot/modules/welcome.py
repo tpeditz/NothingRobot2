f05e6bd5054ad8af7f0e84db5dcdbe87eed5a8af
tpeditz <131423217+tpeditz@users.noreply.github.com>
2023-10-09 20:11:20 +0530

    Update welcome.py

--- NekoRobot/modules/welcome.py
+++ NekoRobot/modules/welcome.py
@@ -9,7 +9,7 @@ from pyrogram.enums import *
 from NekoRobot import pbot
 
 #ADD ANY BUTTON YOU WANT BELOW YOUR WELCOME IMAGE
-markup=InlineKeyboardMarkup ([[InlineKeyboardButton ("Dᴇᴠ", url="https://t.me/Infinityuncounted01")]])
+markup=InlineKeyboardMarkup ([[InlineKeyboardButton ("Dᴇᴠ", url="https://t.me/Tp_Editz")]])
 
 @pbot.on_message(filters.new_chat_members & filters.group)
 async def welcomepic(_, message):
